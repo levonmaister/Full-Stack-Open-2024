dfdfc3a6536e652e2a918eee19a0502ba27e719b
Levon Lopez-Eknosyan <thecactusplayer8@gmail.com>
2024-03-06 08:25:46 +0200

    unicafe finished final version

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,20 +8,53 @@ const Button = ({onSmash, text}) =>(
 
 )
 
+const Statistics = ({list}) => {
+
+if(list.clickobj>0){return(<div>
+  <StatisticLine text={"good"} value={list.goodobj}/>
+  <StatisticLine text={"neutral"} value={list.neutralobj}/>
+  <StatisticLine text={"bad"} value={list.badobj}/>
+  <StatisticLine text={"all"} value={list.allobj}/>
+  <StatisticLine text={"average"} value={(list.goodobj-list.badobj)/list.allobj}/>
+  <StatisticLine text={"positive"} value={(list.goodobj/list.allobj)*100 + " %"}/>
+</div>)}
+else{
+  return(<div>No feedback given</div>)
+}
+
+}
+
+const StatisticLine = ({text, value}) => (
+  <table>
+    <tbody>
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
+    </tbody>
+  </table>
+)
+
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [NumberofClicks, setClicks] = useState(0)
+  const all = good+bad+ neutral
 
 
-  const badfunction = () => {
-    console.log("function was called")
+  const information = {
+    goodobj: good
+    ,neutralobj: neutral
+    ,badobj: bad
+    ,clickobj: NumberofClicks
+    ,allobj: all
   }
 
   const increasecount = (variable, setfunction) => {
-    console.log("adding +1" )
+    setClicks(NumberofClicks+1)
     setfunction(variable+1)
   }
 
@@ -32,9 +65,7 @@ const App = () => {
       <Button onSmash={() => increasecount(neutral,setNeutral)} text={"neutral"}/>
       <Button onSmash={() => increasecount(bad,setBad)} text={"bad"}/>
       <h1>Statistics</h1>
-          <div>good {good}</div>
-          <div>neutral {neutral}</div>
-          <div>bad {bad}</div>
+      <Statistics list={information}/>
     </div>
   )
 }
